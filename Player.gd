f61699f43705fc9c3ccb4678517ddafceec8137b
Gawkat <maximumname@gmail.com>
2019-01-26 00:26:29 +0100

    player update

--- Player.gd
+++ Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export (int) var speed = 300;
+export (int) var speed = 50;
 
 var velocity = Vector2()
 
@@ -21,10 +21,11 @@ func get_input():
 		velocity.x += 1
 	velocity = velocity.normalized() * speed
 	
-	if Input.is_action_pressed('player_interact'):
+	if Input.is_action_just_pressed('player_interact'):
 		for body in $InteractArea.get_overlapping_bodies():
 			if body.is_in_group("npc"):
 				body.interact_with()
+				break
 
 func _process(delta):
 	get_input()
