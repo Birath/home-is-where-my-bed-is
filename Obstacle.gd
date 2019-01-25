dd4e2d90022e74d0c6a82b015ef539194914d558
Gawkat <maximumname@gmail.com>
2019-01-25 23:51:10 +0100

    obstacle

--- Obstacle.gd
+++ Obstacle.gd
@@ -1,13 +1,27 @@
 extends StaticBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var obstacles = {
+	"thingy" : [[Rect2(0, 0, 8, 8), Color("00FFFF")]]
+}
+
+var width = 4
+var height = 4
+var obstacle_bounds = Rect2(0, 0, width, height)
+
+var displacement;
+var obstacle_type;
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	# Pick random obstacle
+	obstacle_type = obstacles.keys()[randi()%obstacles.size()]
+
+	displacement = obstacle_bounds.size / 2
+	$ObstacleShape.shape.extents = displacement
+
+func _draw():
+	for shape in obstacles[obstacle_type]:
+		shape[0].position -= displacement
+		draw_rect(shape[0], shape[1], true)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
