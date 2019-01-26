1700b5594c021b8037529dc52a2ba62359ee85a6
Max Helmrich <maxhe938@student.liu.se>
2019-01-26 01:26:45 +0100

    Player physics fix

--- Player.gd
+++ Player.gd
@@ -5,8 +5,6 @@ export (int) var speed = 50;
 var velocity = Vector2()
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
 	pass
 
 func get_input():
@@ -27,6 +25,16 @@ func get_input():
 				body.interact_with()
 				break
 
-func _process(delta):
+func _physics_process(delta):
 	get_input()
 	move_and_slide(velocity)
+
+func _draw():
+	var shoulder_color = Color(randf(), randf(), randf(), 1)
+	var shoulder = Rect2(-1.25, -0.375, 2.5, 0.75)
+	
+	var head_color = shoulder_color.contrasted()
+	var head = Rect2(-0.5, -0.5, 1, 1)
+	
+	draw_rect(shoulder, shoulder_color, true)
+	draw_rect(head, head_color, true)
